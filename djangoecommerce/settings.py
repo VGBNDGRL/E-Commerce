c1f8a0a4659d2692e1270f46eb8de128f5853924
andrew <andrewvu0203@gmail.com>
2020-11-10 20:01:31 -0600

    simple djangoeccomerceweb set up

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'hello_world',
     'projects',
+    'blog',
+    'djangoecommerceweb'
 ]
 
 MIDDLEWARE = [
---
+++ djangoecommerceweb/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# ~ Others
+class Cart(models.Model):
+    cart_id = models.IntegerField()
+
+class Order(models.Model): 
+    order_id = models.IntegerField()
+
+# class DeliveryProcess(models.Model) : 
+#     order_id = models.ForeignKey(Order)
+
+# ~ Payment
+class ShoppingHistory(models.Model) :
+    shopping_id = models.IntegerField()
+
+# ~ Product 
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=20)
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=20)
+    product_description = models.TextField()
+    product_image = models.FilePathField(path="/img")
+
+# ~ Users
+class Shopper(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=20)
+    user_image = models.FilePathField(path="/img")
+    credit_card = models.IntegerField()
+    # shoppinghistory models.ForeignKey(ShoppingHistory)
+
+class Manager(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=20)
+    user_image = models.FilePathField(path="/img")
+
+class StorageMaintainer(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=20)
+    user_image = models.FilePathField(path="/img")
+
+class DeliveryDriver(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=20)    
+    user_image = models.FilePathField(path="/img")
